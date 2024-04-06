30a14e517f7313b0c10257d0f32ab585e474eb59
djamal1502 <djamal1502@gmail.com>
2024-04-07 00:45:13 +0530

    new

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, GradientBoostingClassifier
 import pickle
 import numpy as np
 
@@ -9,7 +10,17 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+rf = RandomForestClassifier()
+gb = GradientBoostingClassifier()
+
+# Adding a VotingClassifier classifier with multiple classifiers to boost the score.
+model = VotingClassifier(
+    estimators=[
+        ('rf', rf), 
+        ('gb', gb)
+    ],
+).fit(X, y)
+
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
